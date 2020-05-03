815abec3b18614e593b8efe99c1713694e9ab176
Yuri-MRQ <64756384+Yuri-MRQ@users.noreply.github.com>
2020-05-03 20:49:24 -0300

    Take of some coments and update the saudation

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -10,14 +10,13 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data!\n')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = ''
     while city == '' or city.lower() != 'Chicago'.lower() or city.lower() != 'New York'.lower() or city.lower() != 'Washington'.lower():
